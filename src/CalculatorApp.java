63b89dca2e65700e70b4c15482735ec430dc878b
Daniel Otwori <otworijr.03@gmail.com>
2026-03-28 00:56:46 +0300

    Accepting users input

--- src/CalculatorApp.java
+++ src/CalculatorApp.java
@@ -12,7 +12,8 @@ public class CalculatorApp {
 
         System.out.print("Enter your first desired number: ");
         num1=scanner.nextDouble();
-        System.out.print("Enter an operator(+, -, *, /, ^ : ");
+        System.out.print("Enter an operator(+, -, *, /, ^ ):  ");
+
         operator=scanner.next().charAt(0);
         System.out.print("Enter your second desired number: ");
         num2=scanner.nextDouble();
