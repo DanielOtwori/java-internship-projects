e1f33fb4ff0d9cf684be79471d2f65497be692d3
Daniel Otwori <otworijr.03@gmail.com>
2026-03-31 00:26:42 +0300

    Refactored banking system using OOP with BankAccount class

--- src/level2/bankingapplication/BankAccount.java
+++ src/level2/bankingapplication/BankAccount.java
@@ -1,4 +1,29 @@
 package level2.bankingapplication;
 
 public class BankAccount {
+
+    private double balance=0;
+
+public void deposit(double amount) {
+    if (amount <= 0) {
+        System.out.println("Amount entered must be more than 0");
+    } else {
+        balance += amount;
+        System.out.println("Your deposit is Successful.");
+    }
+}
+    public void withdraw(double amount){
+    if(amount<=0){
+        System.out.println("Enter amount that is more then 0");
+    } else if (amount>balance) {
+        System.out.println("Insufficient Funds! It's not looking good bruv \uD83D\uDE2D");
+        //Customized emoji to accompany insufficient balance statement
+    }else{
+        balance-=amount;
+        System.out.println("Withdrawal is Successful");
+    }
+    }
+    public double getBalance(){
+    return balance;
+    }
 }
--- src/level2/bankingapplication/BankingApp.java
+++ src/level2/bankingapplication/BankingApp.java
@@ -1,8 +1,62 @@
 package level2.bankingapplication;
 
-public class BankingApp {
-    public static void main(String[] args){
+import java.util.Scanner;
 
-        System.out.println("Getting started on Banking System");
+public class BankingApp {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        BankAccount account = new BankAccount();
+        boolean isRunning = true;
+        int choice;
+
+        while (isRunning) {
+
+            System.out.println("-------------------------------------");
+            System.out.println("\uD835\uDC7A\uD835\uDC84\uD835\uDC9A\uD835\uDC8D\uD835\uDC8D\uD835\uDC82 \uD835\uDC69\uD835\uDC82\uD835\uDC8F\uD835\uDC8C\uD835\uDC8A\uD835\uDC8F\uD835\uDC88 \uD835\uDC7A\uD835\uDC9A\uD835\uDC94\uD835\uDC95\uD835\uDC86\uD835\uDC8E");
+            System.out.println("-------------------------------------");
+            System.out.println("1. Show Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.println("-------------------------------------");
+
+            System.out.print("Enter your choice (1-4): ");
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1 -> showBalance(account);
+                case 2 -> deposit(account);
+                case 3 -> withdraw(account);
+                case 4 -> isRunning = false;
+                default -> System.out.println("INVALID CHOICE");
+            }
+        }
+//Personalized font for printing out thank you statement
+        System.out.println("------------------------------------------");
+        System.out.println("\uD835\uDC7B\uD835\uDC89\uD835\uDC82\uD835\uDC8F\uD835\uDC8C \uD835\uDC9A\uD835\uDC90\uD835\uDC96 \uD835\uDC87\uD835\uDC90\uD835\uDC93 \uD835\uDC96\uD835\uDC94\uD835\uDC8A\uD835\uDC8F\uD835\uDC88 \uD835\uDC7A\uD835\uDC84\uD835\uDC9A\uD835\uDC8D\uD835\uDC8D\uD835\uDC82 \uD835\uDC69\uD835\uDC82\uD835\uDC8F\uD835\uDC8C\uD835\uDC8A\uD835\uDC8F\uD835\uDC88 \uD835\uDC7A\uD835\uDC9A\uD835\uDC94\uD835\uDC95\uD835\uDC86\uD835\uDC8E");
+        System.out.println("------------------------------------------");
+        scanner.close();
+    }
+
+    // Show balance using BankAccount
+    static void showBalance(BankAccount account) {
+        System.out.println("-------------------------------------");
+        System.out.printf("Balance: $%.2f\n", account.getBalance());
+    }
+
+    // Deposit using BankAccount
+    static void deposit(BankAccount account) {
+        System.out.print("Enter amount to be deposited: ");
+        double amount = scanner.nextDouble();
+        account.deposit(amount);
+    }
+
+    // Withdraw using BankAccount
+    static void withdraw(BankAccount account) {
+        System.out.print("Enter amount to withdraw: ");
+        double amount = scanner.nextDouble();
+        account.withdraw(amount);
     }
 }
